0cd209ccf6d2885bdb2db58e0d56e9bd607dd651
LakshithaV <lakshithavirajkarunarathna@gmail.com>
2025-04-08 13:15:57 +0530

    Update

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Services.css';
 
 const services = [
-  { name: 'Tarpaulin Printing', image: '/images/service1.jpg' },
-  { name: 'Invitation Cards', image: '/images/service2.jpg' },
-  { name: 'Acrylic Printing', image: '/images/service3.jpg' },
-  { name: 'Sticker Printing', image: '/images/service4.jpg' },
+  { name: 'Tarpaulin Printing', image: 'https://dezynedit.com/wp-content/uploads/2022/05/TARPAULIN-PRINTING-PER-SQFT.png' },
+  { name: 'Invitation Cards', image: 'https://static.vecteezy.com/system/resources/previews/027/477/475/non_2x/luxury-wedding-invitation-cards-set-free-vector.jpg' },
+  { name: 'Acrylic Printing', image: 'https://www.pictorem.com/images/acr_intro4.jpg' },
+  { name: 'Sticker Printing', image: 'https://www.rolanddg.eu/-/media/roland-emea/images/applications/stickers/stickers-22/sticker-printing-print-and-cut-vinyl-stickers.jpg/?la=en&h=534&w=800&hash=C565C5CD8356AB7A130D5219E9B785D4' },
 ];
 
 const Services = () => (
