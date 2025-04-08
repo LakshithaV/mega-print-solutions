941df8b928211be9ed11291734250c15c72a5def
LakshithaV <lakshithavirajkarunarathna@gmail.com>
2025-04-08 13:08:48 +0530

    New update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Services from './components/Services';
@@ -18,7 +18,8 @@ const App = () => (
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/mega-print-solutions" element={<Home />} /> {/* Ensure route for landing page */}
+          <Route path="/" element={<Home />} /> {/* Also ensure "Home" route */}
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
